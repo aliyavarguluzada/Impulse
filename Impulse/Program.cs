6a5df38133b5d6097f57615bc80a5eb87884ce0b
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-09-05 15:07:46 +0400

    HomeController

--- Impulse/Program.cs
+++ Impulse/Program.cs
@@ -1,6 +1,26 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Impulse.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+
+    options.UseSqlServer(builder.Configuration["Database:Connection"]));
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie();
+
+var app = builder.Build();
+app.MapControllerRoute("default",
+    "{controller=Home}/{action=Index}");
+
+
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
+
 
 app.Run();
