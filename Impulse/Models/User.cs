3731b269af8d2a9cc420cec9dd383be0aa0ac81f
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-09-05 15:09:57 +0400

    User phone number is optional

--- Impulse/Models/User.cs
+++ Impulse/Models/User.cs
@@ -7,7 +7,7 @@
         //public int CompanyId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public string Phone { get; set; }
+        public string? Phone { get; set; }
         public byte[] Password { get; set; }
         public DateTime Created { get; set; }
         public DateTime Updated { get; set; }
