7fc01d8cead95382654da0e012cbf5902bf55dc7
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-09-05 20:01:07 +0400

    Deleted Unneeded Classes

--- Impulse/Models/User.cs
+++ Impulse/Models/User.cs
@@ -3,15 +3,13 @@
     public class User
     {
         public int Id { get; set; }
-        public int UserStatusId { get; set; }
-        //public int CompanyId { get; set; }
         public int UserRoleId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string? Phone { get; set; }
         public byte[] Password { get; set; }
-        public DateTime Created { get; set; }
-        public DateTime Updated { get; set; }
+        public DateTime? Created { get; set; }
+        public DateTime? Updated { get; set; }
         public UserRole UserRole { get; set; }
     }
 }
