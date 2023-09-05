b332053fda72b6e3911e93ab7edabf94eef2261b
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-09-05 19:57:50 +0400

    Created UserRole

--- Impulse/Models/User.cs
+++ Impulse/Models/User.cs
@@ -5,11 +5,13 @@
         public int Id { get; set; }
         public int UserStatusId { get; set; }
         //public int CompanyId { get; set; }
+        public int UserRoleId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string? Phone { get; set; }
         public byte[] Password { get; set; }
         public DateTime Created { get; set; }
         public DateTime Updated { get; set; }
+        public UserRole UserRole { get; set; }
     }
 }
