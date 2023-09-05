60e6ab41366817bd62870d446bff181ce5d39277
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-09-05 12:43:07 +0400

    Added JobTypes

--- Impulse/Models/JobType.cs
+++ Impulse/Models/JobType.cs
@@ -2,5 +2,10 @@
 {
     public class JobType
     {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? Created { get; set; }
+        public DateTime? Updated { get; set; }
+
     }
 }
