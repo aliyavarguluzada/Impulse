e25c971cfa83c96a6a6b53512084c0edb41de281
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-09-05 12:43:58 +0400

    Added WorkForms

---
+++ Impulse/Models/WorkForm.cs
@@ -0,0 +1,10 @@
+﻿namespace Impulse.Models
+{
+    public class WorkForm
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? Created { get; set; }
+        public DateTime? Updated { get; set;}
+    }
+}
