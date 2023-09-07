f1b199335eab0eaac5b5222dc94578f7860e5a59
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-09-07 11:45:42 +0400

    a few fixes

--- Impulse/Models/Company.cs
+++ Impulse/Models/Company.cs
@@ -6,10 +6,12 @@
         public int UserId { get; set; }
         public string Name { get; set; }
         public string Logo { get; set; }
-        public string Social { get; set; }
+        public int CompanySocialId { get; set; }
         public DateTime? Created { get; set; }
         public DateTime? Updated { get; set; }
         public User User { get; set; }
+        public CompanySocial CompanySocial { get; set; }
+        public List<CompanySocial> CompanySocials { get; set; }
         public List<Vacancy> Vacancies { get; set; }
     }
 }
