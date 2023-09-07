4f91c17c7733136a0177c60171201f8df16c9d42
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-09-07 19:55:37 +0400

    Home Index

--- Impulse/Controllers/HomeController.cs
+++ Impulse/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Impulse.Data;
+using Impulse.DTOs.SiteSettings;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Impulse.Controllers
 {
@@ -9,11 +11,21 @@ namespace Impulse.Controllers
 
         public HomeController(ApplicationDbContext context)
         {
-            context = _context;
+            _context = context;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var siteSettings = _context.SiteSettings.ToList();
+            var siteSettings = await _context
+                .SiteSettings
+                .Select(c => new SiteSettingsHomeIndexDto
+                {
+                    MainImage = c.MainImage,
+                    Title = c.Title,
+                    SecondaryImage = c.SecondaryImage,
+                    Description = c.Description
+                }).ToListAsync();
+
+
             // Dto dan almaq lazimdi melumatlari Select ele Dto yarat
             return View(siteSettings);
         }
---
+++ Impulse/DTOs/SiteSettings/SiteSettingsHomeIndexDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Impulse.DTOs.SiteSettings
+{
+    public class SiteSettingsHomeIndexDto
+    {
+        public string MainImage { get; set; }
+        public string SecondaryImage { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
