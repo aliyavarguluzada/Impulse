9b55d9df8919c52d6961b6d7d2342e3807cadf77
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-09-07 16:19:22 +0400

    3 pages done staticly need to make them dynamic others still remain...

---
+++ Impulse/Controllers/Catalog.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Impulse.Controllers
+{
+    public class Catalog : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
