2376b20a619ef10717f76c91ca22e7936e3b343c
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-09-07 11:46:15 +0400

    .

--- Impulse/Models/CompanySocial.cs
+++ Impulse/Models/CompanySocial.cs
@@ -4,9 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int CompanyId { get; set; }
         public DateTime? Created { get; set; }
         public DateTime? Updated { get; set; }
-        public Company Company { get; set; }
     }
 }
