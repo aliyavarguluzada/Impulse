96d047197c1d70ce2c068feea660eb6ddb027cfd
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-09-07 16:31:22 +0400

    Dtos must be created do not pull info directly from database

--- Impulse/Controllers/HomeController.cs
+++ Impulse/Controllers/HomeController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Impulse.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Impulse.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            context = _context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var siteSettings = _context.SiteSettings.ToList();
+            // Dto dan almaq lazimdi melumatlari Select ele Dto yarat
+            return View(siteSettings);
         }
 
     }
