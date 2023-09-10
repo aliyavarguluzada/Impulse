eda52d0235185a823f77e1da1526300593ddb083
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-09-10 21:58:52 +0400

    Areas company and admin

---
+++ Impulse/Areas/Company/Controllers/CompanyController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Impulse.Areas.Company.Controllers
+{
+    public class CompanyController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
