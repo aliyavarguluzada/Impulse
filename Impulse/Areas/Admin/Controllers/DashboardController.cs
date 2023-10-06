d884c586ffe993ccedab4ae8641c7baffad559d4
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-10-06 23:48:13 +0400

    refactored code

--- Impulse/Areas/Admin/Controllers/DashboardController.cs
+++ Impulse/Areas/Admin/Controllers/DashboardController.cs
@@ -1,6 +1,8 @@
 ﻿using Impulse.Core.Requests;
 using Impulse.Data;
+using Impulse.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
 
 namespace Impulse.Areas.Admin.Controllers
 {
@@ -8,7 +10,7 @@ namespace Impulse.Areas.Admin.Controllers
     public class DashboardController : Controller
     {
         private readonly ApplicationDbContext _context;
-        
+
 
         public DashboardController(ApplicationDbContext context)
         {
@@ -26,14 +28,73 @@ namespace Impulse.Areas.Admin.Controllers
         public async Task<IActionResult> Index(OptionsRequest request)
         {
 
-            if (!ModelState.IsValid)
+            using (var transaction = _context.Database.BeginTransaction())
             {
-                return View(request);
+                try
+                {
+
+                    if (!ModelState.IsValid)
+                    {
+                        return View(request);
+                    }
+
+                    var optCity = new City()
+                    {
+                        Name = request.JobType
+                    };
+
+                    var optJobType = new JobType()
+                    {
+                        Name = request.JobType
+                    };
+
+                    var optJobCategory = new JobCategory()
+                    {
+                        Name = request.JobCategory
+                    };
+
+                    var optWorkForm = new WorkForm()
+                    {
+                        Name = request.WorkForm
+                    };
+
+                    var optEducation = new Education()
+                    {
+                        Name = request.Education
+                    };
+
+                    var optExperience = new Experience()
+                    {
+                        Name = request.Experience
+                    };
+
+                    //await _context.AddAsync(optCity);
+                    //await _context.AddAsync(optJobType);
+                    //await _context.AddAsync(optJobCategory);
+                    //await _context.AddAsync(optWorkForm);
+                    //await _context.AddAsync(optEducation);
+                    //await _context.AddAsync(optExperience);
+
+                    var objectsToCreate = new object[] { optCity, optJobType, optJobCategory, optWorkForm, optEducation, optExperience };
+
+                    foreach (var obj in objectsToCreate)
+                    {
+                        await _context.AddAsync(obj);
+                    }
+
+                    await _context.SaveChangesAsync();
+
+                    await transaction.CommitAsync();
+
+                    return Ok("Options created successfully.");
+
+
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                }
             }
-
-
-
-
 
             return RedirectToAction("index");
         }
--- Impulse/Core/Requests/OptionsRequest.cs
+++ Impulse/Core/Requests/OptionsRequest.cs
@@ -5,26 +5,26 @@ namespace Impulse.Core.Requests
     public class OptionsRequest
     {
         [SafeString]
-        string? JobType { get; set; }
+        public string? JobType { get; set; }
 
         [SafeString]
 
-        string? JobCategory { get; set; }
+        public string? JobCategory { get; set; }
 
         [SafeString]
 
-        string? WorkForm { get; set; }
+        public string? WorkForm { get; set; }
 
         [SafeString]
-        string? City { get; set; }
-
-        [SafeString]
-
-        string? Education { get; set; }
+        public string? City { get; set; }
 
         [SafeString]
 
-        string? Experience { get; set; }
+        public string? Education { get; set; }
+
+        [SafeString]
+
+        public string? Experience { get; set; }
 
     }
 }
