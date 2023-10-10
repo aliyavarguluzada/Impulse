3bbc7424451e7ab9f430efee66a40e7fb41796b1
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-10-10 13:03:00 +0400

    Removed uneeded context from HomeController

--- Impulse/Controllers/HomeController.cs
+++ Impulse/Controllers/HomeController.cs
@@ -10,49 +10,10 @@ namespace Impulse.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ApplicationDbContext _context;
 
-        public HomeController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
         public async Task<IActionResult> Index()
         {
 
-            //var siteSettings = await _context
-            //    .SiteSettings
-            //    .Select(c => new SiteSettingsHomeIndexDto
-            //    {
-            //        MainImage = c.MainImage,
-            //        Title = c.Title,
-            //        SecondaryImage = c.SecondaryImage,
-            //        Description = c.Description
-            //    }).ToListAsync();
-
-            //var cvs = await _context
-            //    .Cvs
-            //    .Select(c => new CvsDto
-            //    {
-            //        CvId = c.Id,
-            //        MainPage = c.MainPage,
-            //        ImageName = c.ImageName
-            //    }).OrderBy(c => c.CvId).Take(3).ToListAsync();
-
-            //var secondarySettings = await _context
-            //    .SecondarySiteSettings
-            //    .Select(c => new SecondarySiteSettingsDto
-            //    {
-            //        DescName = c.DescName,
-            //        TitleName = c.TitleName,
-            //        Description = c.Description
-            //    }).ToListAsync();
-
-            //var vm = new HomeIndexVm
-            //{
-            //    siteSettings = siteSettings,
-            //    Cvs = cvs,
-            //    SecondarySiteSettings = secondarySettings
-            //};
 
             return View();
         }
