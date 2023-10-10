02345889ed1afba6d7affa6e875fec6e3fc18368
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-10-10 21:54:06 +0400

    Added EducationsViewComponent

---
+++ Impulse/Areas/Company/Components/EducationsViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Impulse.Data;
+using Impulse.DTOs.CompanyInfo;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Impulse.Areas.Company.Components
+{
+    public class EducationsViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EducationsViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var educations = await _context
+               .Educations
+               .Select(c => new EducationDto
+               {
+                   EducationId = c.Id,
+                   EducationName = c.Name
+
+               }).ToListAsync();
+
+            return View(educations);
+        }
+    }
+}
